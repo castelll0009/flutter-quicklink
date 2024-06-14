01df76df49672c9434d2dbe3f901c51579fb7c1a
Ivan Castillo <45212708+castelll0009@users.noreply.github.com>
2024-06-13 23:51:15 -0400

    quicklink

--- .dart_tool/dartpad/web_plugin_registrant.dart
+++ .dart_tool/dartpad/web_plugin_registrant.dart
@@ -10,6 +10,7 @@ import 'package:cloud_firestore_web/cloud_firestore_web.dart';
 import 'package:firebase_analytics_web/firebase_analytics_web.dart';
 import 'package:firebase_auth_web/firebase_auth_web.dart';
 import 'package:firebase_core_web/firebase_core_web.dart';
+import 'package:firebase_performance_web/firebase_performance_web.dart';
 import 'package:flutter_keyboard_visibility_web/flutter_keyboard_visibility_web.dart';
 import 'package:google_sign_in_web/google_sign_in_web.dart';
 import 'package:share_plus/src/share_plus_web.dart';
@@ -24,6 +25,7 @@ void registerPlugins([final Registrar? pluginRegistrar]) {
   FirebaseAnalyticsWeb.registerWith(registrar);
   FirebaseAuthWeb.registerWith(registrar);
   FirebaseCoreWeb.registerWith(registrar);
+  FirebasePerformanceWeb.registerWith(registrar);
   FlutterKeyboardVisibilityPlugin.registerWith(registrar);
   GoogleSignInPlugin.registerWith(registrar);
   SharePlusWebPlugin.registerWith(registrar);
